68c6bba43ef49925bb8723ced9a0178d6b4e918a
KineBergseth <kinber96@gmail.com>
2020-10-18 23:55:59 +0200

    oh yeah

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import requests
-from datetime import datetime
 from datetime import date
 from pycoingecko import CoinGeckoAPI  # https://github.com/man-c/pycoingecko
 from datetime import datetime
@@ -10,7 +9,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash_table import DataTable
 from dash.dependencies import Input, Output, State
-import json
 
 cg = CoinGeckoAPI()
 
@@ -34,7 +32,6 @@ def getIds():
     return ids
 
 
-# TODO REWRITE
 def get_accounts():
     token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik0wTXhNVEEzTVVFeU5rRkZSREZGTnpSRE1VUXdPVVU0TXpBNFF6QkdRVVF6UkVSRE1VSTNSUSJ9.eyJpc3MiOiJodHRwczovL3N0YWNjeC5ldS5hdXRoMC5jb20vIiwic3ViIjoiejQ0RGdzWVFKREtyNTRCcmZvU0x3c2pLWWRtUDRLdWtAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vc3RhY2MuZmludGVjaCIsImlhdCI6MTYwMTIwNTAwNywiZXhwIjoxNjAzNzk3MDA3LCJhenAiOiJ6NDREZ3NZUUpES3I1NEJyZm9TTHdzaktZZG1QNEt1ayIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.fBvgUGcc1zS3eStbdGo19mLC6KqOdMeBdo_xuZEBz9jCzllRfrgqIhPbys5Se2XreGxu5_6oKWlXbqDOvnbuvjTJKnhoO9Aom1meUjqbQgaROeN0hbmPxVDKF-JDtOdZbAWtZv1ds9bWF0zqo9Z7ogicZ0eUi8FnEA2h2I6peVQPL9cJJwSfhjXPW73Ws4e6c0vynnhXLc5BcQgst0iaMZd4n3tdruzP_bgEY5GqbKvJxHjL2KNHh933VZSZdx_7mf4imsgsed2AL1QkIkqj5lvf_niyzrEmOLs_K_rSOZqRzO0c1u9wxrCK7qlryzpv8nz3C3zXfNdnMQHOejOFpQ"
     url = "https://fintech-webinar-2020-api.vercel.app/api/accounts"
@@ -96,7 +93,6 @@ def get_price_history(coin, days):
     price_history = cg.get_coin_market_chart_by_id(coin, "usd", days)
 
     # convert time from UNIX
-    # TODO REWRITE
     timestamps = [price[0] / 1000 for price in price_history['prices']]
     dates = [datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S') for timestamp in timestamps]
     prices = [price[1] for price in price_history['prices']]
@@ -164,7 +160,6 @@ app.layout = html.Div(children=[
     html.Label('Select a coin to display data about'),
     generate_ddl_coin(),
     html.Div(id='graph'),
-    # TODO ADD DAY SLIDER????
     html.Div(dcc.Slider(
         id='slider',
         min=1,
